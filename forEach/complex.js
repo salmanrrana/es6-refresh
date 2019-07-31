5b1158fad8200e95fa2a09b7a08b40ca1757c6b5
Salman Rana <salman.r.rana@gmail.com>
2019-07-31 11:42:09 -0500

    complete exercises

--- forEach/complex.js
+++ forEach/complex.js
@@ -7,9 +7,16 @@ var numbers = [1, 2, 3, 4, 5];
 var sum = 0;
 
 // loop over the array, incrementing the sum variable
-numbers.forEach(function(number) {
+// numbers.forEach(function(number) {
+//   sum += number;
+// });
+
+// Doing the above with a helper function adder
+function adder(number) {
   sum += number;
-});
+}
+
+numbers.forEach(adder);
 
 // print the sum variable
 sum;
